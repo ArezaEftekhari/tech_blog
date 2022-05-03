7e46f8d113f8a7735f1cfd92f93c9d331fe80b82
ArezaEftekhari <rezaubii81@gmail.com>
2022-05-03 15:49:00 +0430

    first

---
+++ lib/my_strings.dart
@@ -0,0 +1,13 @@
+class MyStrings {
+
+ static const String viewMoreBlog= "مشاهده داغ ترین نوشته ها";
+ static const String viewMorePodcast= "مشاهده داغ ترین پادکست ها";
+ static const String relatedArticles= "مشاهده داغ ترین نوشته ها";
+ static const String submitArticles= "ارسال مقاله ";
+ static const String submitPodcasts= "ارسال پادکست";
+
+
+
+
+
+}
